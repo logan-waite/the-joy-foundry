60759bdf8f0c06c82433f0577af8631a1d4db6f4
Logan Waite <lwaite2010@gmail.com>
2019-05-01 23:07:15 -0600

    prepare testing for adding things

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
     <h2>Welcome</h2>
+    <div v-for="(product, index) in products" :key="index">
+      <h1>{{product.title}}</h1>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    asyncData: async ({ app, route, payload }) => {
-      console.log(await app.$content('/products').getAll())
-    // return {
-    //   page: (await app.$content("/pages").get(route.path)) || payload
-    // };
+  asyncData: async ({ app, route, payload }) => {
+    console.log(await app.$content("/products").getAll());
+    return {
+      products: (await app.$content("/products").getAll()) || payload
+    };
   }
 };
 </script>
